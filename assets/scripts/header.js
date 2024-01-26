7295cbd9cc7b15f25dec3a33b8ece07f0e5b9b1f
bnfcj <67569485+bnfcj@users.noreply.github.com>
2024-01-26 20:03:39 +0800

    should have fix resizing bugs
    menu buttons work nicely now when resizing,
    filter and mobile menu removed when size increases

--- assets/scripts/header.js
+++ assets/scripts/header.js
@@ -1,33 +1,50 @@
-'use strict'
-class Header{
-    addDropdownEvent(){
-        const menuElement = document.querySelector('.nav__menu')
-        const menuImageElements = document.querySelectorAll('.nav__menu img')
-        const filterElement = document.querySelector('.body-filter')
-        const dropdownElement = document.querySelector('.nav__links')
-        menuElement.addEventListener('click', function(){
-            menuImageElements.forEach(element=> element.classList.toggle('no-display'))
-            filterElement.classList.toggle('body-filter--active')
-            dropdownElement.classList.toggle('nav__links--mobile-expanded')
-            dropdownElement.classList.toggle('nav__links--mobile')
-        })
+"use strict";
+class Header {
+  addDropdownEvent() {
+    const menuElement = document.querySelector(".nav__menu");
+    const filterElement = document.querySelector(".body-filter");
+    const menuImageElements = document.querySelectorAll(".nav__menu img");
+    const dropdownElement = document.querySelector(".nav__links");
+    menuElement.addEventListener("click", function () {
+      menuImageElements.forEach((element) =>
+        element.classList.toggle("no-display")
+      );
+      filterElement.classList.toggle("body-filter--active");
+      dropdownElement.classList.toggle("nav__links--mobile-expanded");
+      dropdownElement.classList.toggle("nav__links--mobile");
+    });
+  }
+  addResizeEvent() {
+    const dropdownElement = document.querySelector(".nav__links");
+    const menuImageElements = document.querySelectorAll(".nav__menu img");
+    const filterElement = document.querySelector(".body-filter");
+    if (window.innerWidth >= 600) {
+      dropdownElement.classList.remove("nav__links--mobile");
     }
-    addResizeEvent(){
-        const dropdownElement = document.querySelector('.nav__links')
-        if (window.innerWidth >=600){
-            dropdownElement.classList.remove('nav__links--mobile')
+    function resize() {
+      // dropdownElement.classList.add('nav__links--mobile')
+
+      dropdownElement.classList.add("nav__links--mobile");
+      if (window.innerWidth >= 600) {
+        menuImageElements.forEach((element,index) =>{
+           if(index===0){
+            element.classList.remove('no-display')
+           }
+           else{
+            element.classList.add('no-display')
+           }
+
         }
-        function resize(){
-            // dropdownElement.classList.add('nav__links--mobile')
-            dropdownElement.classList.add('nav__links--mobile')
-            if (window.innerWidth >=600){
-                dropdownElement.classList.remove('nav__links--mobile')
-            }
-        }
-        window.addEventListener('resize', resize ,false);
-        
+        );
+        filterElement.classList.remove("body-filter--active");
+        dropdownElement.classList.remove("nav__links--mobile-expanded");
+
+        dropdownElement.classList.remove("nav__links--mobile");
+      }
     }
+    window.addEventListener("resize", resize, false);
+  }
 }
-const header = new Header()
-header.addDropdownEvent()
-header.addResizeEvent()
+const header = new Header();
+header.addDropdownEvent();
+header.addResizeEvent();
